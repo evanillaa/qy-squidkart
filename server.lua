421a9f137a689e46ce4e142c8b3ba9c1c232a986
QY242 <48966862+qy242@users.noreply.github.com>
2021-10-20 12:03:14 +0300

    Esx versiyonu eklendi.

--- server.lua
+++ server.lua
@@ -1,3 +1,4 @@
+-- QBCore için
 QBCore = nil
 TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
 
@@ -11,5 +12,22 @@ QBCore.Commands.Add("squidcard", "Etkinlik kartı verir", {}, false, function(so
 end, 'admin')
 
 QBCore.Functions.CreateUseableItem('game_ticket', function(source, item)
-	TriggerClientEvent('qy:squidcard:open', source, item.info.cardnumber)
+    TriggerClientEvent('qy:squidcard:open', source, item.info.cardnumber)
 end)
+
+-- ESX için
+-- ESX = nil
+-- TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+-- RegisterCommand('squidcard', function(source, args)
+--     local xPlayer = ESX.GetPlayerFromId(source)
+--     local number = args[1]
+--     local info = {
+--		cardnumber = number,
+-- 	}
+--     xPlayer.addInventoryItem("game_ticket", 1, nil, info)
+-- end)
+
+-- ESX.RegisterUsableItem('game_ticket', function(source, item)
+--     TriggerClientEvent('qy:squidcard:open', source, item.info.cardnumber)
+-- end)
